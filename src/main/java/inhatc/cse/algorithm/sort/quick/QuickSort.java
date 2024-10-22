2ab00f83608b4165a16f3a44a3ea6052354da9f1
MINJUN YU <minjoon0524@gmail.com>
2024-10-22 20:18:34 +0900

    feat:QuickSort

---
+++ src/main/java/inhatc/cse/algorithm/sort/quick/QuickSort.java
@@ -0,0 +1,58 @@
+package inhatc.cse.algorithm.sort.quick;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    private static void quickSort(int[] arr, int low, int high) {
+        if(low < high) {
+            int pivot = partition(arr,low,high);
+            System.out.println("피벗 : "+ pivot);
+            for (int i:arr){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            System.out.println("\n");
+
+            quickSort(arr,low,pivot-1);
+            quickSort(arr,pivot+1,high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int i = low+1;
+        int j=high;
+
+        while(i<=j){
+            if(arr[i]<=arr[low]){
+                i++;
+            }else if(arr[j]>arr[low]){
+                j--;
+            }else{
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        int t = arr[low];
+        arr[low] = arr[j];
+        arr[j] = t;
+        return j;
+    }
+
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+        int[] arr={15,22,13,27,12,10,20,25};
+        //Arrays.stream(arr).forEach(System.out::println);
+
+        qs.quickSort(arr,0,arr.length-1);
+
+        for (int i:arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+    }
+
+}
